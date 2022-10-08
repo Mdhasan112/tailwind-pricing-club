e316ecabd30b45f4465c1bfb298666f61a380842
hasan <mdhasanhk.98@gmail.com>
2022-10-08 23:58:33 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-     <h1 className='text-6xl font-bold underline'>Hello World</h1>
+      <Header></Header>
+     <h1 className='text-6xl font-bold underline'>Hello World!</h1>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '../Link/Link';
+import { Bars3Icon, XMarkIcon} from '@heroicons/react/24/solid'
 
 const Navbar = () => {
+    const[open, setOpen] = useState(false);
     const routes = [
-        {id: 1, name: "home", link: "/home"},
-        {id: 1, name: "Shop", link: "/Shop"},
-        {id: 1, name: "Deals", link: "/Deals"},
-        {id: 1, name: "Coupons", link: "/Coupons"},
-        {id: 1, name: "Contact", link: "/Contact"},
+        { id: 1, name: "home", link: "/home" },
+        { id: 2, name: "Shop", link: "/Shop" },
+        { id: 3, name: "Deals", link: "/Deals" },
+        { id: 4, name: "Coupons", link: "/Coupons" },
+        { id: 5, name: "Contact", link: "/Contact" },
+        {id: 6, name: "services", link: "/Service"}
     ]
     return (
         <nav>
-            <ul>
+            <div onClick={()=> setOpen(!open)} className='w-6 h-6 md:hidden'>
+                { open ? <XMarkIcon></XMarkIcon> : <Bars3Icon></Bars3Icon>}
+            </div>
+            <ul className={`md: flex justify-center ${open ?'top-6':'top-[-120px]'}`}>
                 {
-                    routes.map(route => <Link route={route} key={route.id}></Link>)
+                    routes.map(route =>
+                        <Link 
+                        route={route} key={route.id}>
+                        </Link>)
                 }
             </ul>
         </nav>
